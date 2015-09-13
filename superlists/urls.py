f3e112024c85402255df2f0e247d05e087f7f852
yghong <todqka83@gmail.com>
2015-09-13 15:14:15 +0900

    add '/lists/new' url.

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/aa/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
 )
