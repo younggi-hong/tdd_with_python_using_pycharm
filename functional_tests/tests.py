0c6a2da5831b7cdbfceec672f47ce82b45265072
yghong <todqka83@gmail.com>
2015-09-13 13:37:58 +0900

    FT testcase 를 추가함.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,6 +38,8 @@ class NewVisitorTest(LiveServerTestCase):
         # enter 를 입력하면, 페이지는 업데이트 되고 "1: Buy milk" 란 to-do 리스트가
         # 생성 된다.
         inputbox.send_keys(Keys.ENTER)
+        mira_list_url = self.browser.current_url
+        self.assertRegex(mira_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy milk')
 
         # "Sing a song" 을 추가한다.
@@ -49,4 +51,30 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Sing a song')
 
-        self.fail('Finish the test!')
+        # self.fail('Finish the test!')
+
+        # 새로운 사용자인 yghong 이 사이트에 방문할 것이다.
+        # 새로운 브라우저를 생성해서 미라 쿠키 정보 등이 나타나지 않도록 한다.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # yghong 방문 했다. 미라의 정보가 없어야 한다.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('Buy milk', page_text)
+        self.assertNotIn('Sing a song', page_text)
+
+        # yghong 이 새로운 리스트를 등록한다.
+        inputbox = self.browser.find_elements_by_id('id_new_item')
+        inputbox.send_keys('Buy book')
+        inputbox.send_keys(Keys.ENTER)
+
+        # yghong 은 자신의 url 을 얻는다.
+        yghong_list_url = self.browser.current_url
+        self.assertRegex(yghong_list_url, '/lists/.+')
+        self.assertNotEqual(yghong_list_url, mira_list_url)
+
+        # mira 정보가 있는지 확인한다.
+        page_text = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('Buy milk', page_text)
+        self.assertIn('Buy book', page_text)
