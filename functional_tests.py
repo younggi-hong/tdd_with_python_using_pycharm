9a098b4dfd64bb2ade253c46d0a6bdfc333fc86b
yghong <todqka83@gmail.com>
2015-09-13 11:59:13 +0900

    refactoring FT.

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # 미라는 멋진 to-do 서비스가 있다는 것을 듣고 사이트에 방문 하였다.
@@ -32,17 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         # enter 를 입력하면, 페이지는 업데이트 되고 "1: Buy milk" 란 to-do 리스트가
         # 생성 된다.
         inputbox.send_keys(Keys.ENTER)
-
-        # import time
-        # time.sleep(10)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy milk' for row in rows),
-            "New to-do item did not appear in table -- its text was:\n%s"
-            % (table.text)
-        )
+        self.check_for_row_in_list_table('1: Buy milk')
 
         # "Sing a song" 을 추가한다.
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -50,11 +45,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 등록된 item 을 확인한다.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy milk', [row.text for row in rows])
-        self.assertIn('2: Sing a song', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Sing a Song')
 
         self.fail('Finish the test!')
 
