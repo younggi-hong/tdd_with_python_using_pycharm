7bed390fb6c6a36e6fff30831891f01da4130b09
yghong <todqka83@gmail.com>
2015-09-15 05:10:39 +0900

    set STATIC_ROOT

--- superlists/settings.py
+++ superlists/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -82,5 +82,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
