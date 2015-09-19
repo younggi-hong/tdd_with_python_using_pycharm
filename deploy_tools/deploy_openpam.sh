4fc75e72c96a2c977ef6a1c7761c3df65b5c551c
yghong <todqka83@gmail.com>
2015-09-20 07:20:11 +0900

    openpam.com staging.openpam.com 을 위한 배포 스크립트를 추가함.

---
+++ deploy_tools/deploy_openpam.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+fab deploy --host=ubuntu@openpam.com -i /Users/yghong/development/projects/mamushi/workspace/aws_ec2/keypair/yghong_wiki.pem
+
--- deploy_tools/run_deploy.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-fab deploy --host=ubuntu@openpamphlets.com -i /Users/yghong/development/projects/mamushi/workspace/aws_ec2/keypair/yghong_wiki.pem
-
