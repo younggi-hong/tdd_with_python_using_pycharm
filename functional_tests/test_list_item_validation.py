876f37a174dbc3233e72e5d187b2b73876f2602f
yghong <todqka83@gmail.com>
2015-09-20 10:40:11 +0900

    adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,12 +6,31 @@ class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
         # mira 는 내용을 입력하지 않고 입력창에서 Enter 를 누른다.
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
         # 홈 페이지가 리프레쉬 되고 에러 메시지가 출력된다.
         # "아이템은 빈칸일 수 없습니다."
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
         # 그래서 입력창에 내용을 입력 후 Enter 를 누른다. 정상 동작한다.
+        self.browser.find_element_by_id('id_new_item').send_keys("Buy milk\n")
+        self.check_for_row_in_list_table('1: Buy milk')
+
         # 다시 두번째 아이템도 빈칸으로 입력한다.
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
         # 비슷한 경고 메시지가 나타난다.
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
         # 입력창에 내용을 채우고 Enter 를 누른다.
         # 정상 동작한다.
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
+
 
         self.fail("write me!")
