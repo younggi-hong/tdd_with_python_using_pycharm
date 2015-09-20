f144f0e08346668277e597188b046fb347c988c7
yghong <todqka83@gmail.com>
2015-09-20 22:01:55 +0900

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -31,3 +31,19 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
+
+    def test_cannot_add_duplicate_items(self):
+
+        # mira 는 리스트를 추가한다.
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Buy bread\n')
+        self.check_for_row_in_list_table('1: Buy bread')
+
+        # 같은 리스트를 추가한다.
+        self.get_item_input_box().send_keys('Buy bread')
+
+        # 중복 추가하면 안된다는 메시지를 확인 한다.
+        self.check_for_row_in_list_table('1: Buy bread')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You've already got this in your list")
+
---
+++ lists/migrations/0004_list_item_unique_together.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0003_auto_20150913_0632'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='item',
+            options={'ordering': ('id',)},
+        ),
+        migrations.AlterUniqueTogether(
+            name='item',
+            unique_together=set([('list', 'text')]),
+        ),
+    ]
