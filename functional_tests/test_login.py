2ea2c741aaae369091674edcd3a9ffba8dcc5d0a
yghong <todqka83@gmail.com>
2015-09-29 17:23:27 +0900

    chapter15 is finished...

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -13,6 +13,8 @@ class LoginTest(FunctionalTest):
         while retries > 0:
             for handle in self.browser.window_handles:
                 self.browser.switch_to_window(handle)
+
+                print("%s" % self.browser.title)
                 if text_in_title in self.browser.title:
                     return
             retries -= 1
@@ -29,7 +31,8 @@ class LoginTest(FunctionalTest):
     def test_login_with_persona(self):
 
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
+
         self.switch_to_new_window('Mozilla Persona')
 
         self.browser.find_element_by_id('authentication_email').send_keys('edith@mockmyid.com')
@@ -37,7 +40,7 @@ class LoginTest(FunctionalTest):
 
         self.switch_to_new_window('To-Do')
 
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
 
--- superlists/settings.py
+++ superlists/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,6 +83,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'superlists', 'static'),
+)
+
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
